0cd0b04ded7598e69a859a76d7e704d21708ccf3
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-02-08 11:17:15 -0500

    feat: icon bar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,23 +9,42 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // MaterialApp es el widget que configura el tema, el titulo y la pantalla inicial
     return MaterialApp(
-      title: 'ejemplo row',
+      title: 'ejemplo mix',
       home: Scaffold(
-        appBar: AppBar(title: const Text("ejemplo row"),),
-        body: Center(
+        appBar: AppBar(title: const Text("barra de iconos")),
+        body: Container(
+          color: Colors.black,
+          height: double.infinity,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Text("elemento A"),
-              
-              ElevatedButton(onPressed: null, child: Text("btn")),
-              
-              Text("elemento b")
-            ],    
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildButtonColumn(Icons.call, 'Llamar'),
+              _buildButtonColumn(Icons.near_me, 'Ruta'),
+              _buildButtonColumn(Icons.share, 'Compartir'),
+            ],
           ),
         ),
-      )
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: Colors.blue),
+        SizedBox(height: 8), // espacio entre icon y text
+        Text(
+          label, 
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: Colors.blue
+          ),
+        ),
+      ],
     );
   }
 }
