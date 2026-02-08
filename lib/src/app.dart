dfefc5c82a5f963372feab8e735aa30e8806c152
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-02-08 12:24:27 -0500

    feat: ScaffoldMessenger

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,26 +1,63 @@
 import 'package:flutter/material.dart';
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ejemplo SingleChildScrollView',
-      home: Scaffold(
-          appBar: AppBar(title: const Text('Widget SingleChildScrollView')),
-          body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal, // Habilita el scroll horizontal
-          child: Row(
-            children: [
-              // Tus widgets aquí
-              Container(width: 200, height: 100, color: Colors.red),
-              Container(width: 200, height: 100, color: Colors.green),
-              Container(width: 200, height: 100, color: Colors.blue),
-              Container(width: 200, height: 100, color: Colors.black),
-            ],
+      title: 'ScaffoldMessenger Demo',
+      home: Builder(
+        // Builder es necesario para obtener un contexto que tenga un Scaffold
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: const Text('ScaffoldMessenger Demo')),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('¡SnackBar simple!'),
+                      ),
+                    );
+                  },
+                  child: const Text('Mostrar SnackBar Simple'),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Elemento eliminado.'),
+                        action: SnackBarAction(
+                          label: 'Deshacer',
+                          onPressed: () {
+                            print('Acción deshecha!');
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text('Mostrar SnackBar con Acción'),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context)
+                      ..removeCurrentSnackBar()
+                      ..showSnackBar(
+                        const SnackBar(content: Text('¡Este es un nuevo mensaje!')),
+                      );
+                  },
+                  child: const Text('Reemplazar SnackBar'),
+                ),
+              ],
+            ),
           ),
-        )
+        ),
       ),
     );
   }
