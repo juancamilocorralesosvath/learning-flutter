e95cc74c889064652becf530cf43ad7e086d464e
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-02-08 10:42:37 -0500

    feat: container widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,11 +9,22 @@ class App extends StatelessWidget {
     // MaterialApp es el widget que configura el tema, el titulo y la pantalla inicial
     return MaterialApp(
       title: 'First app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("ejemplo container"),),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+              child: Text(
+                "hello container!",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),    
+          ),
+        ),
+      )
     );
   }
 }
