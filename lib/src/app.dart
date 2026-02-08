6016dd52b34d0c56e3c5e2ed1341a409bf721a0b
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-02-08 12:21:52 -0500

    feat: SingleChildScrollView

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,32 +6,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ejemplo Image Widget',
+      title: 'Ejemplo SingleChildScrollView',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Widget Image')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('Imagen desde Internet:'),
-              const SizedBox(height: 10),
-              Image.network(
-                'https://yt3.googleusercontent.com/2__G-ckA66-4JgXPlHTGZvg8CoUIgDU6qYFnJqW-AsVeJvBRT4hCjXz4XMOjIqm4m7v431lT=s900-c-k-c0x00ffffff-no-rj',
-                width: 250,
-                height: 250,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(height: 30),
-              const Text('Imagen desde Assets:'),
-              const SizedBox(height: 10),
-              Image.asset(
-                'assets/logo.jpg', 
-                width: 200, 
-                height: 200
-              ),
+          appBar: AppBar(title: const Text('Widget SingleChildScrollView')),
+          body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal, // Habilita el scroll horizontal
+          child: Row(
+            children: [
+              // Tus widgets aquí
+              Container(width: 200, height: 100, color: Colors.red),
+              Container(width: 200, height: 100, color: Colors.green),
+              Container(width: 200, height: 100, color: Colors.blue),
+              Container(width: 200, height: 100, color: Colors.black),
             ],
           ),
-        ),
+        )
       ),
     );
   }
